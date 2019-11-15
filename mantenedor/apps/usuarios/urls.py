12dfc407523529b6904accb14e540d796dbac0e2
matias <aaa@a.cl>
2019-11-15 20:47:02 -0300

    Implimentacion formulario/registro, cambio style y navbar

--- mantenedor/apps/usuarios/urls.py
+++ mantenedor/apps/usuarios/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 
-
 urlpatterns = [   
-    #path('', views.main_page),
+    path('registro', views.Registro.as_view()),
+    path('registro_exitoso', views.registro_exitoso),
 ]
--- mantenedor/apps/usuarios/views.py
+++ mantenedor/apps/usuarios/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from .forms import RegistroForm
 # Create your views here.
+class Registro(CreateView):
+	model = User
+	template_name = "usuario/registro.html"
+	form_class = RegistroForm
+	success_url = "/accounts/registro_exitoso"
+
+def registro_exitoso(request):
+    return render(request, 'usuario/registro_exitoso.html', {})
