3557795a36c14156c7395eccfa03bf9e2c6a14cc
Chtrisian <a@a>
2019-12-03 20:19:24 -0300

    Recuperar contrase;a

--- mantenedor/Serial/urls.py
+++ mantenedor/Serial/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth.views import LoginView
+from django.urls import path, re_path, include
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.productos.urls')),
     path('accounts/', include('apps.usuarios.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    #path('reset/', LoginView.as_view(template_name='main_page.html', success_url=reverse_lazy('adopcion:solicitud_listar')), name = 'login'),
+    path('reset/password_reset/', PasswordResetView.as_view(template_name='registration/password_reset_forms.html', email_template_name="registration/password_reset_email.html"), name = 'password_reset'),
+    path('reset/password_reset_done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name = 'password_reset_done'),
+    re_path(r'^reset/(?P<uidb64>[0-9A-za-z_\-]+)/(?P<token>.+)/$', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirms.html'), name = 'password_reset_confirm'),
+    path('reset/done',PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html') , name = 'password_reset_complete'),
 ]
